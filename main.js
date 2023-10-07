dea4d3b122cc093c9c84bf899fbfb0513d28b032
r3t3p <92801240+PgGitau@users.noreply.github.com>
2023-10-07 18:25:56 +0300

    minor edits

--- main.js
+++ main.js
@@ -1,101 +1,99 @@
 (function() {
-  "use strict";
+  ("use strict");
 
-  //  Initialize Swiper 
-		var swiper = new Swiper('.hero', {
-      // slidesPerView: 1,
-      spaceBetween: 30,
-      effect: "fade",      
-      loop: true,
-      autoplay: {
-        delay: 5000,
-        pauseOnMouseEnter: true,
-        disableOnInteraction: false,
-       },
-      // loopFillGroupWithBlank: true,
-      // pagination: {
-      //   el: '.swiper-pagination',
-      //   clickable: true,
-      // },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+  //  Initialize Swiper
+  var swiper = new Swiper(".hero", {
+    // slidesPerView: 1,
+    spaceBetween: 30,
+    effect: "fade",
+    loop: true,
+    // autoplay: {
+    //   delay: 5000,
+    //   pauseOnMouseEnter: true,
+    //   disableOnInteraction: false,
+    //  },
+    // loopFillGroupWithBlank: true,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+  var swiper = new Swiper(".farmer-list", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    loop: true,
+    loopFillGroupWithBlank: true,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    //   clickable: true,
+    // },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is 425px - 640px
+      425: {
+        slidesPerView: 2,
+        spaceBetween: 10,
       },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      }
-    });
-
-
-    var swiper = new Swiper('.farmer-list', {
-      slidesPerView: 1,
-      spaceBetween: 10,
-      
-      loop: true,
-      loopFillGroupWithBlank: true,
-      // pagination: {
-      //   el: '.swiper-pagination',
-      //   clickable: true,
-      // },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+      // when window width is 640px - 900px
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20,
       },
-			breakpoints: {     
-			// when window width is 425px - 640px
-			 425: {       
-         slidesPerView: 2,
-         spaceBetween: 10     
-       },    
-      // when window width is 640px - 900px    
-       640: {       
-         slidesPerView: 3,       
-         spaceBetween: 20     
-       },   
-  
-      // when window width is >= 900px     
-      //  900: {       
-      //    slidesPerView: 4,       
-      //    spaceBetween: 30     
-      //  } 
-      }
-    });
 
+      // when window width is >= 900px
+      //  900: {
+      //    slidesPerView: 4,
+      //    spaceBetween: 30
+      //  }
+    },
+  });
 
-    var swiper = new Swiper(".popular-swiper", {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      loop: true,
-      autoplay: {
-        delay: 3000,
-        pauseOnMouseEnter: true,
-        disableOnInteraction: false
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev"
-      }
-    });
-  
+  var swiper = new Swiper(".popular-swiper", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
       easing: "ease-in-out",
       once: true,
-      mirror: false
+      mirror: false,
     });
   });
 
   /**
-   * Initiate Pure Counter 
+   * Initiate Pure Counter
    */
   new PureCounter();
 
@@ -103,50 +101,74 @@
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener("scroll", listener);
+  };
 
-   /**
+  /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-   let selectHeader = select('#header')
-   if (selectHeader) {
-     const headerScrolled = () => {
-       if (window.scrollY > 100) {
-         selectHeader.classList.add('header-scrolled')
-       } else {
-         selectHeader.classList.remove('header-scrolled')
-       }
-     }
-     window.addEventListener('load', headerScrolled)
-     onscroll(document, headerScrolled)
-   }
+  let selectHeader = select("#header");
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add("header-scrolled");
+      } else {
+        selectHeader.classList.remove("header-scrolled");
+      }
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
+  }
 
-  
+  // Get the modal
+  var modal = document.getElementById("myModal");
+
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+
+  // When the user clicks the button, open the modal
+  btn.onclick = function () {
+    modal.style.display = "block";
+  };
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 })() 
