2387a5f81a5e842181c37711194ab9b543dd34c0
unknown <githinjipete21@gmail.com>
2023-10-13 18:47:52 +0300

    minor edits

--- main.js
+++ main.js
@@ -129,7 +129,7 @@
 
   // Get the button that opens the modal
   var btn = document.getElementById("myBtn");
-  var btn2 = document.getElementById("myBtn2");
+  var cartbtn = document.getElementById("cartbtn");
 
   // Get the <span> element that closes the modal
   var span = document.getElementsByClassName("close")[0];
@@ -139,8 +139,9 @@
     modal.style.display = "block";
   };
 
-  btn2.onclick = function () {
+  cartbtn.onclick = function () {
     modal.style.display = "block";
+    console.log("im clicked")
   };
 
   // When the user clicks on <span> (x), close the modal
